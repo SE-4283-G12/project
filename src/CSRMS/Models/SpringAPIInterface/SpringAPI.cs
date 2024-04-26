6b98296589f5923c3314d3fb0012c2d29c55aac3
Monikrishna2288 <rayalamonikrishna@gmail.com>
2024-04-25 19:03:49 -0500

    Added SpringAPI.cs for the interfacing of the application
    Added the functions to the class SpringAPI

--- src/CSRMS/Models/SpringAPIInterface/SpringAPI.cs
+++ src/CSRMS/Models/SpringAPIInterface/SpringAPI.cs
@@ -13,15 +13,32 @@ namespace CSRMS.Models.SpringAPIInterface
         {
 
         }
+		
+		public void login(string accountId, string password)
+		{
+			
+		}
+		
+		public void updateAccount(string session_sessionId, string account_accountId, string account_administrator, string account_firstName, string account_lastName, string account_emailAddress, string account_password)
+		{
+			
+		}
 
         public void deleteAccount(string sessionId, string accountId)
         {
         }
-
-        public void deleteAccount(string sessionId, string accountId, string password)
-        {
-        }
-
+		
+		public void logoffAccount(string sessionId, string accountId)
+		{
+			
+		}
+		
+		public void getAccountDetails(string sessionId, string accountId)
+		{
+			
+		}
+		
+		
 
 
         // Task Methods
