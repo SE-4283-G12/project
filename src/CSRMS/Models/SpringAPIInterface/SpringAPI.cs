8a0b415fe97d0c46224bddd7ae7c71969c2dec61
Logan <Logangosha@gmail.com>
2024-04-26 09:42:31 -0500

    Page-Backend-Logic Commit 1
    Added Create Account Page
    Added logic to Landing Page

--- src/CSRMS/Models/SpringAPIInterface/SpringAPI.cs
+++ src/CSRMS/Models/SpringAPIInterface/SpringAPI.cs
@@ -6,89 +6,89 @@ using CSRMS.Models.AccountModel;
 
 namespace CSRMS.Models.SpringAPIInterface
 {
-    public class SpringAPI
+    public static class SpringAPI
     {
         // Account Methods
-        public void createAccount(string accountId, bool isAdministrator, string preferedName, string emailAddress, string password)
+        public static void createAccount(string accountId, bool isAdministrator, string preferedName, string emailAddress, string password)
         {
 
         }
 		
-		public void login(string accountId, string password)
+		public static void login(string accountId, string password)
 		{
 			
 		}
 		
-		public void updateAccount(string session_sessionId, string account_accountId, string account_administrator, string account_firstName, string account_lastName, string account_emailAddress, string account_password)
+		public static void updateAccount(string session_sessionId, string account_accountId, string account_administrator, string account_firstName, string account_lastName, string account_emailAddress, string account_password)
 		{
 			
 		}
 
-        public void deleteAccount(string sessionId, string accountId)
+        public static void deleteAccount(string sessionId, string accountId)
         {
         }
 		
-		public void logoffAccount(string sessionId, string accountId)
+		public static void logoffAccount(string sessionId, string accountId)
 		{
 			
 		}
 		
-		public void getAccountDetails(string sessionId, string accountId)
+		public static void getAccountDetails(string sessionId, string accountId)
 		{
 			
 		}
-		
-		public void createTask(string session_sessionId, string task_id, string task_title, string task_description, string task_priority, string task_location, string task_is_complete, string task_start_date_time, string task_due_date_time, string task_account_id, string task_category)
-		{
-			
-		}
-		
-		public void updateTask(string session_sessionId, string task_id, string task_title, string task_description, string task_priority, string task_location, string task_is_complete, string task_start_date_time, string task_due_date_time, string task_account_id, string task_category[])
-		{
-			
-		}
-		
-		public void deleteTask(string session_sessionId, string task_id)
-		{
-			
-		}
-		public void getTaskDetails(string session_sessionId, string task_id)
-		{
-			
-		}
-		public void getAllTasksOfAccount(string session_sessionId)
-		{
-			
-		}
-		
-		public void createCategory(string session_sessionId, string category_id, string category_categoryName, string category_task[])
-		{
-			
-		}
-		
-		public void updateCategory(string session_sessionId, string category_id, string category_categoryName, string category_task[])
-		{
-			
-		}
-		
-		public void deleteCategory(string session_sessionId, string category_id)
-		{
-			
-		}
-		public void getCategoryDetails(string session_sessionId, string category_id)
-		{
-			
-		}
-		public void getAllCategoriesOfAccount(string session_sessionId)
-		{
-			
-		}
-		
-
-
         // Task Methods
-
+		
+		public static void createTask(string session_sessionId, string task_id, string task_title, string task_description, string task_priority, string task_location, string task_is_complete, string task_start_date_time, string task_due_date_time, string task_account_id, string task_category)
+		{
+			
+		}
+		
+		public static void updateTask(string session_sessionId, string task_id, string task_title, string task_description, string task_priority, string task_location, string task_is_complete, string task_start_date_time, string task_due_date_time, string task_account_id, string task_category[])
+		{
+			
+		}
+		
+		public static void deleteTask(string session_sessionId, string task_id)
+		{
+			
+		}
+		public static void getTaskDetails(string session_sessionId, string task_id)
+		{
+			
+		}
+		public static void getAllTasksOfAccount(string session_sessionId)
+		{
+			
+		}
+		
         // Category Methods
+		public static void createCategory(string session_sessionId, string category_id, string category_categoryName, string category_task[])
+		{
+			
+		}
+		
+		public static void updateCategory(string session_sessionId, string category_id, string category_categoryName, string category_task[])
+		{
+			
+		}
+		
+		public static void deleteCategory(string session_sessionId, string category_id)
+		{
+			
+		}
+		public static void getCategoryDetails(string session_sessionId, string category_id)
+		{
+			
+		}
+		public static void getAllCategoriesOfAccount(string session_sessionId)
+		{
+			
+		}
+		
+
+
+
 
         // Reminder Methods
 
--- src/CSRMS/Pages/LandingPage.aspx.cs
+++ src/CSRMS/Pages/LandingPage.aspx.cs
@@ -4,21 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CSRMS.Models.SpringAPIInterface;
 
 namespace CSRMS
 {
     public partial class _Default : Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
+        { }
         public void getViewAllTaskView()
-        { }
+        {
+            Response.Redirect("ViewAllTasksPage.aspx");
+        }
         public void getAddTaskView()
-        { }
-        public void getEditTaskView()
-        { }
+        {
+            Response.Redirect("AddTaskPage.aspx");
+        }
+        public void getEditProfileView()
+        {
+            Response.Redirect("EditProfileSettingsPage.aspx");
+        }
         public void viewTodaysReminders()
         { }
         public void selectEventsView()
