4be6592432fe714dc4e585955a617aa8846fb9fe
Andrew Hudson <ahudson14@uco.edu>
2024-04-27 21:42:36 -0500

    ANDREW:  created first Test class to learn with

---
+++ src/CSRMS/Tests/UnitTests/AccountModelTests/UserAccountTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+namespace CSRMS.Models.AccountModel
+{
+    public class UserAccountTests
+    {
+        public static void UserAccount_Constructor_ReturnsUserAccount()
+        {
+            try
+            {
+                //Arrange
+                var userAccount = new UserAccount;
+                //Act
+
+                //Assert
+
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+    }
+}
