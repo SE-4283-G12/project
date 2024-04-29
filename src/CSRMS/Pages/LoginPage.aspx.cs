9945d22d4ef21cc28bc7ef935c59c0999557f57a
D-Henrichs <157083879+D-Henrichs@users.noreply.github.com>
2024-04-28 21:17:19 -0500

    comments and corrections

--- src/CSRMS/Pages/LoginPage.aspx.cs
+++ src/CSRMS/Pages/LoginPage.aspx.cs
@@ -14,7 +14,7 @@ namespace CSRMS.Pages
 {
     public partial class LoginPage : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void Page_Load(object sender, EventArgs e)    //Checking for page loading errors
         {
             if (!Page.IsPostBack)
             {
@@ -30,24 +30,20 @@ namespace CSRMS.Pages
                 errorMessage.CssClass = "error_message_label";
             }
         }
-        public void createAccountSelected()
-        {
-            Response.Redirect("CreateAccountPage.aspx");
-        }
 
-        public void loginBtnClicked(object sender, EventArgs e)
+        public void loginBtnClicked(object sender, EventArgs e)     //Attempt login
         {
             sendCredentials(email.Text, password.Text);
             errorCheck();
         }
 
-        public void sendCredentials(string email, string password)
+        public void sendCredentials(string email, string password)      //Send login info for verification
         {
             if(UserAccount.validateCredentials(email, password)) Response.Redirect("LandingPage.aspx");
             else ViewState["ErrorMessage"] = "Invalid Username or Password";
         }
 
-        protected void createUser_Click(object sender, EventArgs e)
+        protected void createUser_Click(object sender, EventArgs e)     //Create new user selection
         {
             Response.Redirect("CreateUser.aspx");
         }
